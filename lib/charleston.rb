2ed3b48c3757779194169a9ee97797c595a6796f
Bryce Kerley <bkerley@brycekerley.net>
2010-10-08 23:40:33 -0400

    call generator

--- lib/charleston.rb
+++ lib/charleston.rb
@@ -3,5 +3,6 @@ require File.join(File.dirname(__FILE__), 'charleston', 'generator')
 module Charleston
   def self.create_project(project_name)
     Dir.mkdir(project_name)
+    Charleston::Generator.new project_name
   end
 end
