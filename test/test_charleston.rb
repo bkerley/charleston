61b80f09fa731336f3d6a83bbbb900f162b90aa1
Bryce Kerley <bkerley@brycekerley.net>
2010-10-08 23:12:56 -0400

    remove command-line tests for now

--- test/test_charleston.rb
+++ test/test_charleston.rb
@@ -10,36 +10,4 @@ class TestCharleston < Test::Unit::TestCase
       end
     end
   end
-
-  context 'the "charleston" utility' do
-    should 'call the Charleston.create_project method with the directory argument' do
-      test_argument = rand(1000).to_s
-      Charleston.expects(:create_project).once.with(test_argument)
-      run_command_line_with_args test_argument
-    end
-
-    should 'puts to stderr at least twice when called with the --help argument' do
-      $stderr.expects(:puts).at_least(2)
-      run_command_line_with_args '--help'
-    end
-  end
-
-  def run_command_line_with_args(*args)
-    with_argv_as args do
-      load File.join(File.dirname(__FILE__), '..', 'bin', 'charleston')
-    end
-  end
-
-  def with_argv_as(args)
-    old_argv = ARGV.dup
-    replace_argv_with args
-    yield
-  ensure
-    replace_argv_with old_argv
-  end
-
-  def replace_argv_with(args)
-    ARGV.clear
-    args.each_index { |i| ARGV[i] = args[i]}
-  end
 end
