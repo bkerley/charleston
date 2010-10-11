8a1c8cb8e2ad12c06584e97bda5d21296bff0aaa
Bryce Kerley <bkerley@brycekerley.net>
2010-10-11 00:46:56 -0400

    transformation rules

---
+++ lib/charleston/transform.rb
@@ -0,0 +1,45 @@
+module Charleston
+  class Transform
+    def initialize(source_directory, source_pattern,
+                   destination_directory, destination_pattern,
+                   &transformation)
+      @source_dir = source_directory
+      @source_pat = source_pattern
+      @dest_dir = destination_directory
+      @dest_pat = destination_pattern
+      @transformation = transformation
+
+      find_sources
+      target_destinations
+      write_rules
+    end
+
+    private
+    def find_sources
+      @sources = ::Rake::FileList[File.join(@source_dir, @source_pat)]
+    end
+
+    def target_destinations
+      src_extension = find_extension_in_pattern(@source_pat)
+      dest_extension = find_extension_in_pattern(@dest_pat)
+      @destinations = @sources.map do |s|
+        File.expand_path(
+                             s.sub(@source_dir, File.join('output', @dest_dir)).
+                             sub(src_extension, dest_extension))
+      end
+    end
+
+    def write_rules
+      @sources.zip(@destinations) do |p|
+        input, output = p
+        file output => [input, 'output'] do
+          @transformation.call input, output
+        end
+      end
+    end
+
+    def find_extension_in_pattern(pattern)
+      pattern.sub(/\*\./, '')
+    end
+  end
+end
--- test/test_transform.rb
+++ test/test_transform.rb
@@ -7,20 +7,28 @@ class TestTransform < Test::Unit::TestCase
       subject { Charleston::Transform.method(:new)}
       should 'work with four arguments' do
         assert_nothing_raised do
-          Charleston::Transform.new 'pages', '*.haml', '', '*.html'
+          Charleston::Transform.new('pages', '*.haml', '', '*.html'){ }
         end
       end
 
       context 'called with a directory full of hamls' do
         setup do
           FakeFS::FileSystem.clone File.join(File.dirname(__FILE__), 'fixture')
-          Charleston::Transform.new 'pages', '*.haml', '', '*.html'
+          FakeFS::FileSystem.chdir File.join(File.dirname(__FILE__), 'fixture')
+          Charleston::Transform.new('pages', '*.haml', '', '*.html'){ }
         end
 
         before_should 'make file rules for index and about' do
           %w{ index about }.each do |f|
-            Charleston::Transform.any_instance.expects(:file).
-              with("output/#{f}.html" => ["pages/#{f}.haml", 'output'])
+            Charleston::Transform.any_instance.expects(:file).at_least_once.with do |args|
+              output = args.keys.first
+              input = args[output]
+              next false if args.length > 1
+              next false unless output =~ Regexp.new("output/#{f}\\\.html$")
+              next false unless input.include? 'output' #directory
+              next false unless input.detect{ |v| v =~ Regexp.new("pages/#{f}\\\.haml$")}
+              true
+            end
           end
         end
       end
