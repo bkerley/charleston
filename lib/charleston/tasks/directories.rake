4a14498e46acd3080e821c90d80d8411569a45a6
Ed Toro <etoro@prisadigital.com>
2011-11-23 15:56:58 -0500

    - Directories task isn't necessary.  Create dirs on-demand. - A separate "transform.generate" call is no longer necessary.  Make the   rules on init. - More graceful handling when coffeescript is missing. - Fix warning messags for Rake 0.9 by importing Rake::DSL - Refactor the transformer - Add support for differently formatted patterns: html, .html, *.html,   etc. - Add support for recursively walking the input dirs - Update the transform test for refactored transformer, pattern testing,   and recursion testing

--- lib/charleston/tasks/directories.rake
+++
@@ -1,8 +0,0 @@
-desc 'Generate output directories'
-task 'generate:directories' => ['output', 'output/stylesheets', 'output/javascripts', 'output/images']
-
-directory 'output'
-directory 'output/stylesheets'
-directory 'output/javascripts'
-directory 'output/images'
-
--- test/test_transform.rb
+++ test/test_transform.rb
@@ -11,44 +11,42 @@ class TestTransform < Test::Unit::TestCase
         end
       end
 
-      context 'called with a directory full of hamls' do
-        setup do
-          @fixture_directory = File.expand_path(File.join(File.dirname(__FILE__), 'fixture'))
-          FakeFS::FileSystem.clone @fixture_directory
-          FakeFS::FileSystem.chdir @fixture_directory
-          @transform = Charleston::Transform.new(File.join(@fixture_directory, 'pages'), '*.haml', '', '*.html'){ }
-        end
-
-        before_should 'make file rules for index and about' do
-          %w{ index about }.each do |f|
-            Charleston::Transform.any_instance.expects(:file).at_least_once.with do |args|
-              output = args.keys.first
-              input = args[output]
-              next false if args.length > 1
-              next false unless output =~ Regexp.new("output/#{f}\\\.html$")
-              next false unless input.include? 'output' #directory
-              next false unless input.detect{ |v| v =~ Regexp.new("pages/#{f}\\\.haml$")}
-              true
-            end
-          end
-        end
-
-        context 'when asked to register rules' do
+      %w{ .haml *.haml haml }.each do |pattern|
+        context "called with a directory full of hamls/htmls and pattern #{pattern}" do
           setup do
-            @transform.generates 'haml'
+            @fixture_directory = File.expand_path(File.join(File.dirname(__FILE__), 'fixture'))
+            FakeFS::FileSystem.clone @fixture_directory
+            FakeFS::FileSystem.chdir @fixture_directory
+            @transform = Charleston::Transform.new(File.join(@fixture_directory, 'pages'), pattern, '', '*.html'){ }
           end
 
-          before_should 'make a "generate:haml" rule and add it to the generate:all rule' do
-            @transform.expects(:task).with do |args|
-              task = args.keys.first
-              depends = args[task]
-              next false unless args.length == 1
-              next false unless task == 'generate:haml'
-              next false unless depends.detect{ |v| v =~ /output\/index\.html$/ }
-              next false unless depends.detect{ |v| v =~ /output\/about\.html$/ }
-              true
+          before_should 'make file rules for all the hamls' do
+            %w{ index about subdir/index }.each do |f|
+              Charleston::Transform.any_instance.expects(:file).at_least_once.with do |args|
+                output = args.keys.first
+                input = args[output]
+                next false if args.length > 1
+                next false unless output =~ Regexp.new("output/#{f}\\\.html$")
+                #next false unless input.include? 'output' #directories are created on demand
+                next false unless input.detect{ |v| v =~ Regexp.new("pages/#{f}\\\.haml$")}
+                true
+              end
             end
-            @transform.expects(:task).with('generate:all'=>'generate:haml')
+          end
+
+          before_should 'make a "generate:html" rule and add it to the generate:all rule' do
+            %w{ index about subdir/index }.each do |f|
+              Charleston::Transform.any_instance.expects(:task).at_least_once.with do |args|
+                task = args.keys.first
+                depends = args[task]
+                next false unless args.length == 1
+                next false unless task == 'generate:html'
+                next false unless depends =~ Regexp.new("output/#{f}\\\.html$")
+                true
+              end
+            end
+            # one for every haml file, which is weird, but ok for now
+            Charleston::Transform.any_instance.expects(:task).times(3).with({"generate:all"=>"generate:html"})
           end
         end
       end
